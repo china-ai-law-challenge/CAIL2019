f6ea9b001e134825b30a973c316e6c6eb10f4c18
Dsir123 <1542981914@qq.com>
2019-05-24 12:54:15 +0800

    Add files via upload

---
+++ 阅读理解/model/main.py
@@ -0,0 +1,9 @@
+#coding: utf-8
+infile = "../data/data.json"
+outfile = "../result/result.json"
+def main():
+    data = open(infile,"r")
+    result = open(outfile,"w")
+
+if __name__ == '__main__':
+    main()
